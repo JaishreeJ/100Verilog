8b968fad4ac02a5786187af7b084717701cacb76
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-02-02 05:00:57 +0530

    Create fs_tb.v

---
+++ fullsub/fs_tb.v
@@ -0,0 +1,21 @@
+module tb_top;
+  reg a, b, Bin;
+  wire D, Bout;
+  
+  full_subtractor fs(a, b, Bin, D, Bout);
+  
+initial begin
+  
+  $dumpfile("tb_top.vcd");
+  $dumpvars(1);
+  $monitor("At time %0t: a=%b b=%b, Bin=%b, difference=%b, borrow=%b",$time, a,b,Bin,D,Bout);
+    a = 0; b = 0; Bin = 0; #1;
+    a = 0; b = 0; Bin = 1; #1;
+    a = 0; b = 1; Bin = 0; #1;
+    a = 0; b = 1; Bin = 1; #1;
+    a = 1; b = 0; Bin = 0; #1;
+    a = 1; b = 0; Bin = 1; #1;
+    a = 1; b = 1; Bin = 0; #1;
+    a = 1; b = 1; Bin = 1;
+  end
+endmodule
