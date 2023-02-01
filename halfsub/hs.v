b4f3944db5bc67b8eec3cebf1715f5fc68342dca
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-02-02 04:30:53 +0530

    Create hs.v

---
+++ halfsub/hs.v
@@ -0,0 +1,8 @@
+module hs(a, b, diff, borrow);
+input a;
+input b;
+output diff;
+output borrow;
+assign borrow=(~a)&b;                           
+assign diff=a^b;                             
+endmodule
