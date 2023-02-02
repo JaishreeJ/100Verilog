55259c33eb18ec0bdf72bde448516256b797f6c1
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-02-03 04:05:47 +0530

    Create synccount_tb.v

---
+++ sequential/Counters/synccounter/synccount_tb.v
@@ -0,0 +1,23 @@
+module synccountertb;
+	reg clk, reset;
+	wire [3:0] count;
+	synccounter synccntr(clk,reset,count);
+initial
+  begin
+		clk = 0;
+		reset = 1;
+		#125;
+		reset = 0;
+		#4500;
+		$finish;
+	end
+	always 
+      #50 clk=~clk;
+	always@(posedge clk)
+	begin
+		$display ("value is %d", count);
+	end
+  initial begin 
+    $dumpfile("dump.vcd"); $dumpvars;
+  end
+endmodule
