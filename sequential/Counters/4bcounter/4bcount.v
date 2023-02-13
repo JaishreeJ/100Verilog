dca1568de6bbd65fec3f663df25b4209192c34b8
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-02-13 23:05:56 +0530

    Create 4bcount.v

---
+++ sequential/Counters/4bcounter/4bcount.v
@@ -0,0 +1,14 @@
+module 4bcounter (
+  output [3:0] out;
+  input enable, clk, reset; 
+                  reg [3:0] out);
+  always @(posedge clk)
+    if (reset)
+      begin
+        out <= 4'b0 ;
+      end 
+  else if (enable)
+    begin
+      out <= out + 1’b1;
+      end
+endmodule 
